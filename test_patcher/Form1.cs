f2f481b696f1f3560f95da302c24c098f2f454dd
13xforever <ilya.veselov@gmail.com>
2018-09-28 13:27:32 +0500

    fixed form layout

--- test_patcher/Form1.cs
+++ test_patcher/Form1.cs
@@ -1,202 +1,159 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace test_patcher
 {
-    public partial class Form1 : Form
-    {
-        string selectedPath = "";
-        string IRDselectedPath = "";
-        string hexString = "";
-        byte[] IRDcompressed;
-        byte[] IRDdecompressed;
-        byte[] ISOkey = new byte[163];
-        
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
 
-        public bool ByteArrayToFile(string _FileName, byte[] _ByteArray)
-        {
-            try
-            {
-                // Open file for reading
-                System.IO.FileStream _FileStream =
-                   new System.IO.FileStream(_FileName, System.IO.FileMode.Open,
-                                            System.IO.FileAccess.Write);
-                // Writes a block of bytes to this stream using data from
-                // a byte array.
-                _FileStream.Seek(3952, SeekOrigin.Begin);
-                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
+		public Form1() { InitializeComponent(); }
 
-                // close file stream
-                _FileStream.Close();
+		private bool ByteArrayToFile(string fileName, byte[] byteArray)
+		{
+			try
+			{
+				// Open file for reading
+				using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Write))
+				{
+					// Writes a block of bytes to this stream using data from
+					// a byte array.
+					fileStream.Seek(3952, SeekOrigin.Begin);
+					fileStream.Write(byteArray, 0, byteArray.Length);
+					// close file stream
+					fileStream.Close();
+					return true;
+				}
+			}
+			catch (Exception exception)
+			{
+				// Error
+				Console.WriteLine("Exception caught in process: " + exception);
+			}
+			// error occured, return false
+			return false;
+		}
 
-                return true;
-            }
-            catch (Exception _Exception)
-            {
-                // Error
-                Console.WriteLine("Exception caught in process: {0}",
-                                  _Exception.ToString());
-            }
+		private static byte[] StringToByteArray(string hex)
+		{
+			return Enumerable.Range(0, hex.Length)
+				.Where(x => x % 2 == 0)
+				.Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+				.ToArray();
+		}
 
-            // error occured, return false
-            return false;
-        }
+		private static byte[] ReadFile(string pathSource)
+		{
+			try
+			{
+				using (var fsSource = new FileStream(pathSource, FileMode.Open, FileAccess.Read))
+				{
+					// Read the source file into a byte array.
+					var bytes = new byte[fsSource.Length];
 
-        public static byte[] StringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
-        }
+					var numBytesToRead = (int)fsSource.Length;
+					var numBytesRead = 0;
+					while (numBytesToRead > 0)
+					{
+						// Read may return anything from 0 to numBytesToRead.
+						var n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
 
-        public static byte[] ReadFile(string pathSource)
-        {
-            try
-            {
-                using (FileStream fsSource = new FileStream(pathSource,
-                    FileMode.Open, FileAccess.Read))
-                {
+						// Break when the end of the file is reached.
+						if (n == 0)
+							break;
 
-                    // Read the source file into a byte array.
-                    byte[] bytes = new byte[fsSource.Length];
+						numBytesRead += n;
+						numBytesToRead -= n;
+					}
+					numBytesToRead = bytes.Length;
+					return bytes;
+				}
+			}
+			catch (FileNotFoundException ioEx)
+			{
+				Console.WriteLine(ioEx.Message);
+				var placeholder = new byte[1];
+				return placeholder;
+			}
+		}
 
-                    int numBytesToRead = (int)fsSource.Length;
-                    int numBytesRead = 0;
-                    while (numBytesToRead > 0)
-                    {
-                        // Read may return anything from 0 to numBytesToRead.
-                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+		private static byte[] Decompress(byte[] data)
+		{
+			using (var compressedStream = new MemoryStream(data))
+			using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+			using (var resultStream = new MemoryStream())
+			{
+				zipStream.CopyTo(resultStream);
+				return resultStream.ToArray();
+			}
+		}
 
-                        // Break when the end of the file is reached.
-                        if (n == 0)
-                            break;
+		private void SelectIsoButtonClick(object sender, EventArgs e)
+		{
+			var dialog = new OpenFileDialog();
+			dialog.ShowDialog();
+			if (!string.IsNullOrEmpty(dialog.FileName))
+				textBox1.Text = dialog.FileName;
+		}
 
-                        numBytesRead += n;
-                        numBytesToRead -= n;
-                    }
-                    numBytesToRead = bytes.Length;
-                    return bytes;
-                }
-            }
-            catch (FileNotFoundException ioEx)
-            {
-                Console.WriteLine(ioEx.Message);
-                byte[] placeholder = new byte[1];
-                return placeholder;
-            }
-        }
-        static byte[] Decompress(byte[] data)
-        {
-            using (var compressedStream = new MemoryStream(data))
-            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
-            using (var resultStream = new MemoryStream())
-            {
-                zipStream.CopyTo(resultStream);
-                return resultStream.ToArray();
-            }
-        }
+		private void PatchButtonClick(object sender, EventArgs e)
+		{
+			var hexString = richTextBox1.Text;
+			var selectedPath = textBox1.Text;
+			if (hexString != "" && selectedPath != "")
+			{
+				if (hexString.Length == 168 || hexString.Length == 326)
+				{
+					ByteArrayToFile(selectedPath, StringToByteArray(hexString));
+					MessageBox.Show("Done!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				else if (hexString.Length != 168 || hexString.Length != 326)
+					MessageBox.Show("Wrong keys length!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			else if (selectedPath == "")
+				MessageBox.Show("No image was selected!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			else
+				MessageBox.Show("No keys was inserted!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.ShowDialog();
+		private void SelectIrdButtonClick(object sender, EventArgs e)
+		{
+			var dialog = new OpenFileDialog();
+			dialog.ShowDialog();
 
-            if (dialog.FileName == "")
-            {
-                MessageBox.Show("No file was selected!", "Load Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (dialog.FileName != "")
-            {
-                selectedPath = dialog.FileName;
-                textBox1.Text = selectedPath;
-            }
-        }
+			if (!string.IsNullOrEmpty(dialog.FileName))
+			{
+				var irDselectedPath = dialog.FileName;
+				textBox2.Text = irDselectedPath;
+				var irDdecompressed = Decompress(ReadFile(irDselectedPath));
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            hexString = richTextBox1.Text;
+				var isOkey = new byte[163];
+				isOkey[0] = 0x45;
+				isOkey[1] = 0x6E;
+				isOkey[2] = 0x63;
+				isOkey[3] = 0x72;
+				isOkey[4] = 0x79;
+				isOkey[5] = 0x70;
+				isOkey[6] = 0x74;
+				isOkey[7] = 0x65;
+				isOkey[8] = 0x64;
+				isOkey[9] = 0x20;
+				isOkey[10] = 0x33;
+				isOkey[11] = 0x4B;
+				isOkey[12] = 0x20;
+				isOkey[13] = 0x42;
+				isOkey[14] = 0x4C;
+				isOkey[15] = 0x44;
 
-            if (hexString != "" && selectedPath != "")
-            {
-                if (hexString.Length == 168 || hexString.Length == 326)
-                {
-                    ByteArrayToFile(selectedPath, StringToByteArray(hexString));
-                    MessageBox.Show("Done!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else if (hexString.Length != 168 || hexString.Length != 326)
-                {
-                    MessageBox.Show("Wrong keys length!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            else if (selectedPath == "")
-            {
-                MessageBox.Show("No image was selected!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (hexString == "")
-            {
-                MessageBox.Show("No keys was inserted!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }          
-        }
+				for (var i = irDdecompressed.Length - 40; i < irDdecompressed.Length - 8; i++)
+					isOkey[i - (irDdecompressed.Length - 40) + 16] = irDdecompressed[i];
+				for (var j = irDdecompressed.Length - 155; j < irDdecompressed.Length - 40; j++)
+					isOkey[j - (irDdecompressed.Length - 155) + 48] = irDdecompressed[j];
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.ShowDialog();
-
-            if (dialog.FileName == "")
-            {
-                MessageBox.Show("No file was selected!", "Load Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (dialog.FileName != "")
-            {
-
-                IRDselectedPath = dialog.FileName;
-                textBox2.Text = IRDselectedPath;
-                IRDcompressed = ReadFile(IRDselectedPath);
-                IRDdecompressed = Decompress(IRDcompressed);
-
-                ISOkey[0] = 0x45;
-                ISOkey[1] = 0x6E;
-                ISOkey[2] = 0x63;
-                ISOkey[3] = 0x72;
-                ISOkey[4] = 0x79;
-                ISOkey[5] = 0x70;
-                ISOkey[6] = 0x74;
-                ISOkey[7] = 0x65;
-                ISOkey[8] = 0x64;
-                ISOkey[9] = 0x20;
-                ISOkey[10] = 0x33;
-                ISOkey[11] = 0x4B;
-                ISOkey[12] = 0x20;
-                ISOkey[13] = 0x42;
-                ISOkey[14] = 0x4C;
-                ISOkey[15] = 0x44;
-
-                for (int i = (IRDdecompressed.Length - 40); i < (IRDdecompressed.Length - 8); i++)
-                {
-                    ISOkey[(i - (IRDdecompressed.Length - 40)) + 16] = IRDdecompressed[i];
-                }
-                for (int j = (IRDdecompressed.Length - 155); j < (IRDdecompressed.Length - 40); j++)
-                {
-                    ISOkey[(j - (IRDdecompressed.Length - 155)) + 48] = IRDdecompressed[j];
-                }
-
-                hexString = BitConverter.ToString(ISOkey).Replace("-", string.Empty);
-                richTextBox1.Text = hexString;
-            }
-        }
-    }
+				richTextBox1.Text = BitConverter.ToString(isOkey).Replace("-", "");
+			}
+		}
+	}
 }
