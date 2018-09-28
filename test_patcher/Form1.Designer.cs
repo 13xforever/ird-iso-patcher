899878bfbb3599e5d455b464e7a581043862bcd5
13xforever <ilya.veselov@gmail.com>
2018-09-28 13:24:40 +0500

    imported original source code for the patcher

---
+++ test_patcher/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace test_patcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "No file selected...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Patch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select ISO image:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Place ISO keys here:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 136);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(232, 128);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "No file selected...";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Select IRD image:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 309);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "PS3 ISO Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ test_patcher/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace test_patcher
+{
+    public partial class Form1 : Form
+    {
+        string selectedPath = "";
+        string IRDselectedPath = "";
+        string hexString = "";
+        byte[] IRDcompressed;
+        byte[] IRDdecompressed;
+        byte[] ISOkey = new byte[163];
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool ByteArrayToFile(string _FileName, byte[] _ByteArray)
+        {
+            try
+            {
+                // Open file for reading
+                System.IO.FileStream _FileStream =
+                   new System.IO.FileStream(_FileName, System.IO.FileMode.Open,
+                                            System.IO.FileAccess.Write);
+                // Writes a block of bytes to this stream using data from
+                // a byte array.
+                _FileStream.Seek(3952, SeekOrigin.Begin);
+                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
+
+                // close file stream
+                _FileStream.Close();
+
+                return true;
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}",
+                                  _Exception.ToString());
+            }
+
+            // error occured, return false
+            return false;
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static byte[] ReadFile(string pathSource)
+        {
+            try
+            {
+                using (FileStream fsSource = new FileStream(pathSource,
+                    FileMode.Open, FileAccess.Read))
+                {
+
+                    // Read the source file into a byte array.
+                    byte[] bytes = new byte[fsSource.Length];
+
+                    int numBytesToRead = (int)fsSource.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+                        // Read may return anything from 0 to numBytesToRead.
+                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                        // Break when the end of the file is reached.
+                        if (n == 0)
+                            break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+                    numBytesToRead = bytes.Length;
+                    return bytes;
+                }
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+                byte[] placeholder = new byte[1];
+                return placeholder;
+            }
+        }
+        static byte[] Decompress(byte[] data)
+        {
+            using (var compressedStream = new MemoryStream(data))
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            using (var resultStream = new MemoryStream())
+            {
+                zipStream.CopyTo(resultStream);
+                return resultStream.ToArray();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+
+            if (dialog.FileName == "")
+            {
+                MessageBox.Show("No file was selected!", "Load Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (dialog.FileName != "")
+            {
+                selectedPath = dialog.FileName;
+                textBox1.Text = selectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hexString = richTextBox1.Text;
+
+            if (hexString != "" && selectedPath != "")
+            {
+                if (hexString.Length == 168 || hexString.Length == 326)
+                {
+                    ByteArrayToFile(selectedPath, StringToByteArray(hexString));
+                    MessageBox.Show("Done!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (hexString.Length != 168 || hexString.Length != 326)
+                {
+                    MessageBox.Show("Wrong keys length!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (selectedPath == "")
+            {
+                MessageBox.Show("No image was selected!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (hexString == "")
+            {
+                MessageBox.Show("No keys was inserted!", "Patch Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+
+            if (dialog.FileName == "")
+            {
+                MessageBox.Show("No file was selected!", "Load Image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (dialog.FileName != "")
+            {
+
+                IRDselectedPath = dialog.FileName;
+                textBox2.Text = IRDselectedPath;
+                IRDcompressed = ReadFile(IRDselectedPath);
+                IRDdecompressed = Decompress(IRDcompressed);
+
+                ISOkey[0] = 0x45;
+                ISOkey[1] = 0x6E;
+                ISOkey[2] = 0x63;
+                ISOkey[3] = 0x72;
+                ISOkey[4] = 0x79;
+                ISOkey[5] = 0x70;
+                ISOkey[6] = 0x74;
+                ISOkey[7] = 0x65;
+                ISOkey[8] = 0x64;
+                ISOkey[9] = 0x20;
+                ISOkey[10] = 0x33;
+                ISOkey[11] = 0x4B;
+                ISOkey[12] = 0x20;
+                ISOkey[13] = 0x42;
+                ISOkey[14] = 0x4C;
+                ISOkey[15] = 0x44;
+
+                for (int i = (IRDdecompressed.Length - 40); i < (IRDdecompressed.Length - 8); i++)
+                {
+                    ISOkey[(i - (IRDdecompressed.Length - 40)) + 16] = IRDdecompressed[i];
+                }
+                for (int j = (IRDdecompressed.Length - 155); j < (IRDdecompressed.Length - 40); j++)
+                {
+                    ISOkey[(j - (IRDdecompressed.Length - 155)) + 48] = IRDdecompressed[j];
+                }
+
+                hexString = BitConverter.ToString(ISOkey).Replace("-", string.Empty);
+                richTextBox1.Text = hexString;
+            }
+        }
+    }
+}
